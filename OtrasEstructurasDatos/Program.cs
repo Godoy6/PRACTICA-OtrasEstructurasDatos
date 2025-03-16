dcf3c27545c36b492ba557f6ad8e9ffc319105bc
Godoy6 <sergiogodoyromero@gmail.com>
2025-03-16 18:31:40 +0100

    18:31 / 16-03-2025
    Primer Commit

---
+++ OtrasEstructurasDatos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OtrasEstructurasDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
